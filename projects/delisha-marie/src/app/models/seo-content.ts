5b9321978ba5221029917c71e6740335ac453109
Clarence Robinson <sirdarquan@gmail.com>
2026-04-24 14:57:19 -0500

    put the new seos in place

--- projects/delisha-marie/src/app/models/seo-content.ts
+++ projects/delisha-marie/src/app/models/seo-content.ts
@@ -1,14 +1,19 @@
-export interface SeoContent {
-  title?: string;
-  description: string;
+export interface SeoContent extends SeoData {
   url: string;
   siteName: string;
-  keywords?: string[];
-  image?: string;
-  imageWidth?: string;
-  imageHeight?: string;
-  imageType?: string;
   type?: string;
   twitterCard?: string;
   content?: string;
 }
+
+export interface SeoData {
+  title: string;
+  description: string;
+  image: string;
+  imageWidth?: string;
+  imageHeight?: string;
+  imageType?: string;
+  keywords?: string[];
+}
+
+export type RecipeList = SeoData;
