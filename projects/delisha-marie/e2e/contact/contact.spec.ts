38dd78c9c9234e6cc4fce0e6756d5c8fe438cb07
Clarence Robinson <sirdarquan@gmail.com>
2026-05-02 08:58:58 -0500

    ok. the e2e test are solid

--- projects/delisha-marie/e2e/contact/contact.spec.ts
+++ projects/delisha-marie/e2e/contact/contact.spec.ts
@@ -11,12 +11,12 @@ test.describe('Contact Page', () => {
   });
 
   test('should fill out the form fields', async ({ page }) => {
-    await page.getByPlaceholder('Delisha Marie').fill('Robot Tester');
-    await page.getByPlaceholder('hello@delishamarie.com').fill('test@example.com');
-    await page.getByPlaceholder('Recipe question').fill('Automated Test');
+    await page.getByPlaceholder('Delisha Marie').pressSequentially('Robot Tester');
+    await page.getByPlaceholder('hello@delishamarie.com').pressSequentially('test@example.com');
+    await page.getByPlaceholder('Recipe question').pressSequentially('Automated Test');
     await page
       .getByPlaceholder('Your beautiful message...')
-      .fill('Testing the structured directory setup.');
+      .pressSequentially('Testing the structured directory setup.');
 
     const submitBtn = page.getByRole('button', { name: 'Send Message' });
     await expect(submitBtn).toBeEnabled();
