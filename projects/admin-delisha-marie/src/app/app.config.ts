a7f8c71d645f581661fa3673467839e1aeb857b0
Clarence Robinson <sirdarquan@gmail.com>
2026-05-11 09:27:16 -0500

    updated to use prodiveAppInitializer, provideAppConfig and provideSocialLogins

--- projects/admin-delisha-marie/src/app/app.config.ts
+++ projects/admin-delisha-marie/src/app/app.config.ts
@@ -1,13 +1,17 @@
 import {
   ApplicationConfig,
+  inject,
+  provideAppInitializer,
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { SOCIAL_AUTH_CONFIG, GoogleLoginProvider } from '@abacritt/angularx-social-login';
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
+import { provideSocialLogins } from './provider/social-logins';
+import { PluginRegistry } from '../core/plugins/plugin-registry.services';
+import { provideAppConfig } from './services/config.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,24 +19,8 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
-    {
-      provide: SOCIAL_AUTH_CONFIG,
-      useValue: {
-        autoLogin: true,
-        lang: 'en',
-        providers: [
-          {
-            id: GoogleLoginProvider.PROVIDER_ID,
-            provider: new GoogleLoginProvider(
-              '644709001057-ocbuepp7f8ol5piivbupa3d1erkk0c6m.apps.googleusercontent.com',
-              {
-                oneTapEnabled: false,
-                autoLogin: true,
-              },
-            ),
-          },
-        ],
-      },
-    },
+    provideAppInitializer(() => inject(PluginRegistry).initAll()),
+    provideAppConfig(),
+    provideSocialLogins(),
   ],
 };
