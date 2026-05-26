3bed349988ac7fae1b4239fd99783efe0f7650d5
Clarence Robinson <sirdarquan@gmail.com>
2026-05-26 18:35:01 -0500

    got rid of an expression type

--- projects/admin-delisha-marie/src/app/app.ts
+++ projects/admin-delisha-marie/src/app/app.ts
@@ -34,7 +34,7 @@ export class App {
   private readonly currentUrl = toSignal(
     this.router.events.pipe(
       filter((e) => e instanceof NavigationEnd),
-      map((e) => (e as NavigationEnd).urlAfterRedirects),
+      map((e) => (e).urlAfterRedirects),
     ),
     { initialValue: this.router.url },
   );
