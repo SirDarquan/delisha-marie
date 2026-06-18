a21ca6943ac95433cbdea0f8089c5a399e744009
Clarence Robinson <sirdarquan@gmail.com>
2026-06-18 12:00:37 -0500

    refactor(admin): simplify UUID generation in recipe service

--- projects/admin-delisha-marie/src/app/services/recipe.service.ts
+++ projects/admin-delisha-marie/src/app/services/recipe.service.ts
@@ -68,14 +68,7 @@ export class RecipeService {
 
   createRecipe(newRecipe: Omit<Recipe, 'id'>): Recipe {
     const all = this._recipes();
-    const id =
-      typeof crypto !== 'undefined' && crypto.randomUUID
-        ? crypto.randomUUID()
-        : 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-            const r = (Math.random() * 16) | 0;
-            const v = c === 'x' ? r : (r & 0x3) | 0x8;
-            return v.toString(16);
-          });
+    const id = crypto.randomUUID();
     const recipe: Recipe = {
       ...newRecipe,
       id,
