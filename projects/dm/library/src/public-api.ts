2123b6040900a9584a3ab776311758217bad23fe
Clarence Robinson <sirdarquan@gmail.com>
2026-07-08 19:27:59 -0500

    refactor: move plugin registry to shared library

--- projects/dm/library/src/public-api.ts
+++ projects/dm/library/src/public-api.ts
@@ -5,3 +5,6 @@
 export * from './lib/stars/stars';
 export * from './lib/types/recipe';
 export * from './lib/utils/url-utils';
+
+export * from './lib/plugins/plugin.token';
+export * from './lib/plugins/plugin-registry.services';
