883807a6d838d4ad82bc8e9aa94b6afce9cdb9df
Clarence Robinson <sirdarquan@gmail.com>
2026-09-05 18:59:54 -0500

    feat: add vercel analytics and speed insights

--- projects/delisha-marie/src/main.ts
+++ projects/delisha-marie/src/main.ts
@@ -1,5 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import { App } from './app/app';
 import { appConfig } from './app/app.config';
 
+inject();
+injectSpeedInsights();
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
