a002615d0c9288647f94e1d5812671d9f5dd5c68
Clarence Robinson <sirdarquan@gmail.com>
2026-09-25 16:53:28 -0500

    style(delisha-home): add sticky header and streamline http client provider

--- projects/delisha-home/src/app/app.config.ts
+++ projects/delisha-home/src/app/app.config.ts
@@ -1,9 +1,9 @@
+import { provideHttpClient } from '@angular/common/http';
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideHttpClient(withFetch()),
+    provideHttpClient(),
     provideRouter(routes),
   ],
 };
